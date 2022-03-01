51ac32e62a9a1f1a106612e6c04dd07f16934438
Infraestructura Angular <A2209985@correo.uia>
2022-02-28 20:17:51 -0600

    Se agrega el prodcuto

---
+++ angular-compras-uia/src/app/reportes-componente/reportes-componente.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReportesComponenteComponent } from './reportes-componente.component';
+
+describe('ReportesComponenteComponent', () => {
+  let component: ReportesComponenteComponent;
+  let fixture: ComponentFixture<ReportesComponenteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReportesComponenteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReportesComponenteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-compras-uia/src/app/reportes-componente/reportes-componente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reportes-componente',
+  templateUrl: './reportes-componente.component.html',
+  styleUrls: ['./reportes-componente.component.css']
+})
+export class ReportesComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
